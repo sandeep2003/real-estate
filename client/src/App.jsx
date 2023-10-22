b0a11bfe3d0bb99f312041a317e945bb2adca24a
sandeep.patel <sandeep.patel@ciphersquuare.tech>
2023-10-22 14:28:18 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+export default function App() {
+  console.log('gijhu');
+  return (
+    <div className='row'>
+      <div className="col-lg-12" >
+        <h1 className='text-red-500'>App</h1>
+      </div>
+    </div>
+    
+  )
+}
