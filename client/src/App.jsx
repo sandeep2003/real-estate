cdc57c366c9e359f4dd23bdc013edcd89aefb224
sandeep.patel <sandeep.patel@ciphersquuare.tech>
2023-10-22 15:26:09 +0530

    all file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,21 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
 
 export default function App() {
-  console.log('gijhu');
-  return (
-    <div className='row'>
-      <div className="col-lg-12"  >
-        <h1 className='text-red-500'>This App</h1>
-      </div>
-    </div>
-    
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/about' element={<About/>}/>
+    </Routes>
+  </BrowserRouter>
 }
+  
